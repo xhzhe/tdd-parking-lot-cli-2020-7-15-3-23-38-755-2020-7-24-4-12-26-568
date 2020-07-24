0006b0bd5c5169e7595dc1a809591d9e5f971563
alex <alex.h.z.xu@oocl.com>
2020-07-24 15:38:31 +0800

    [alex xu] refactor: make ParkingLot return correspond car by ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,7 +1,23 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private Map<Ticket, Car> parkingLot;
+
+    public ParkingLot() {
+        this.parkingLot = new HashMap<>(16);
+    }
+
     public Ticket park(Car car) {
-        return new Ticket();
+        Ticket ticket = new Ticket();
+        this.parkingLot.put(ticket, car);
+        return ticket;
+    }
+
+
+    public Car fetchCar(Ticket ticket) {
+        return this.parkingLot.get(ticket);
     }
 }
