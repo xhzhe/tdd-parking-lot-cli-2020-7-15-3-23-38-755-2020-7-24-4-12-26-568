224af21da677dfe611c5de40af6dda4a2de197d6
alex <alex.h.z.xu@oocl.com>
2020-07-24 17:24:30 +0800

    [alex xu] feature: add should_return_1_when_park_given_car and should_return_10_when_get_capacity_given_parking_lot test

---
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -0,0 +1,29 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.ParkingLot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParkingLotTest {
+    @Test
+    public void should_return_1_when_park_given_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        //when
+        parkingLot.park(new Car());
+        //then
+        assertEquals(1, parkingLot.size());
+    }
+
+    @Test
+    public void should_return_10_when_get_capacity_given_parking_lot() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        //when
+        int capacity = parkingLot.getCapacity();
+        //then
+        assertEquals(10, capacity);
+    }
+}
