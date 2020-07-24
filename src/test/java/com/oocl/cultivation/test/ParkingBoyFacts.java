4022854da5c399032b2059872fc1946432a06724
alex <alex.h.z.xu@oocl.com>
2020-07-24 15:21:17 +0800

    [alex xu] feature: add should_return_ticket_when_parking_given_car test

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -2,9 +2,17 @@ package com.oocl.cultivation.test;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyFacts {
     @Test
-    void write_your_first_test() {
-
+    void should_return_ticket_when_parking_given_car() {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        //when
+        Ticket ticket = parkingLot.park(car);
+        //then
+        assertNotNull(ticket);
     }
 }
