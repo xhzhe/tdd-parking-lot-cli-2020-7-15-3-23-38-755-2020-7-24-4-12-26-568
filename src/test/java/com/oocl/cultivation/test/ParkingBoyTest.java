7032b8f2e8fd0d83acb160106bd84ad94ae7d8a4
alex <alex.h.z.xu@oocl.com>
2020-07-24 16:07:15 +0800

    [alex xu] fix bugs: add parkingBoy variable at should_return_wrong_message_when_ask_parking_boy_given_non_provide_ticket test

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -1,5 +1,6 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.ParkingLot;
 import com.oocl.cultivation.Ticket;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,8 @@ public class ParkingBoyTest {
     public void should_return_wrong_message_when_ask_parking_boy_given_non_provide_ticket() {
         //given
         Ticket ticket = new Ticket();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         //when
         String message = parkingBoy.getReason(ticket);
         //then
