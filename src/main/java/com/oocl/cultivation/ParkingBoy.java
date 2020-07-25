8ec061d2185ec903c045ff63993f86a20361d6e2
xhz <747367352@qq.com>
2020-07-25 11:29:01 +0800

    [alex xu] refactor: change ParkingBoy to store list of parkingLots

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,21 +1,37 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingBoy {
-    private final ParkingLot parkingLot;
+    private final List<ParkingLot> parkingLots;
 
     public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+        this.parkingLots = new ArrayList<>();
+        this.parkingLots.add(parkingLot);
+    }
+
+    public ParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
     }
 
     public Car fetchCar(Ticket ticket) {
-        return this.parkingLot.fetchCar(ticket);
+        for (ParkingLot parkingLot : this.parkingLots) {
+            Car car = parkingLot.fetchCar(ticket);
+            if (car != null) {
+                return car;
+            }
+        }
+        return null;
     }
 
     public Ticket park(Car car) {
-        if (this.parkingLot.size() + 1 > this.parkingLot.getCapacity()) {
-            return null;
+        for (ParkingLot parkingLot : this.parkingLots) {
+            if (!(parkingLot.size() + 1 > parkingLot.getCapacity())) {
+                return parkingLot.park(car);
+            }
         }
-        return this.parkingLot.park(car);
+        return null;
     }
 
     public String getReason(Ticket ticket) {
@@ -27,5 +43,9 @@ public class ParkingBoy {
 
     public String getReason(Car car) {
         return "Not enough position.";
+    }
+
+    public List<ParkingLot> getParkingLots() {
+        return this.parkingLots;
     }
 }
