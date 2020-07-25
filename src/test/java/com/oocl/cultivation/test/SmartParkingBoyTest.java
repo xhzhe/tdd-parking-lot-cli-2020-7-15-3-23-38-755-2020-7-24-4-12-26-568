ac9cd93237bb7f8da16dba2edddbacfab33198a1
xhz <747367352@qq.com>
2020-07-25 12:05:20 +0800

    [alex xu] feature: add should_return_null_when_parking_given_21_cars test

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -1,9 +1,6 @@
 package com.oocl.cultivation.test;
 
-import com.oocl.cultivation.Car;
-import com.oocl.cultivation.ParkingBoy;
-import com.oocl.cultivation.ParkingLot;
-import com.oocl.cultivation.SmartParkingBoy;
+import com.oocl.cultivation.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -27,5 +24,24 @@ public class SmartParkingBoyTest {
         //then
         assertEquals(result.get(0).size(), 5);
         assertEquals(result.get(1).size(), 5);
+    }
+
+    @Test
+    public void should_return_null_when_parking_given_21_cars() {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot());
+        parkingLots.add(new ParkingLot());
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(parkingLots);
+        Ticket ticket = new Ticket();
+        //when
+        for (int times = 0; times < 21; times++) {
+            ticket = parkingBoy.park(new Car());
+        }
+        List<ParkingLot> result = parkingBoy.getParkingLots();
+        //then
+        assertEquals(result.get(0).size(), 10);
+        assertEquals(result.get(1).size(), 10);
+        assertNull(ticket);
     }
 }
