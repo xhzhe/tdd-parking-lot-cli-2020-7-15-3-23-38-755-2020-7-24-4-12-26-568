fa0aca846445396eee01be27b369a0118eedfeee
alex <alex.h.z.xu@oocl.com>
2020-07-27 15:24:14 +0800

    [alex xu] refactor: NormalParkStrategy to use stream

--- src/main/java/com/oocl/cultivation/NormalParkStrategy.java
+++ src/main/java/com/oocl/cultivation/NormalParkStrategy.java
@@ -1,15 +1,14 @@
 package com.oocl.cultivation;
 
 import java.util.List;
+import java.util.Optional;
 
 public class NormalParkStrategy implements ParkStrategy {
     @Override
     public Ticket park(Car car, List<ParkingLot> parkingLots) {
-        for (ParkingLot parkingLot : parkingLots) {
-            if (!(parkingLot.getCarsCount() + 1 > parkingLot.getCapacity())) {
-                return parkingLot.park(car);
-            }
-        }
-        return null;
+        Optional<ParkingLot> optionalParkingLot = parkingLots.stream().
+                filter(parkingLot -> parkingLot.getCarsCount() < parkingLot.getCapacity()).
+                findFirst();
+        return optionalParkingLot.map(parkingLot -> parkingLot.park(car)).orElse(null);
     }
 }
