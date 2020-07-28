a3f778edbaebc1781efdd98ec51cc5f6875f0b9a
alex <alex.h.z.xu@oocl.com>
2020-07-28 10:26:27 +0800

    [alex xu] refactor: extract meaningless string to constant

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -6,6 +6,9 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class ParkingBoy implements Parkable {
+    private static final String NOT_ENOUGH_POSITION = "Not enough position.\n";
+    private static final String UNRECOGNIZED_PARKING_TICKET = "Unrecognized parking ticket.\n";
+    private static final String NONE_PARKING_TICKET = "Please provide your parking ticket.\n";
     protected final List<ParkingLot> parkingLots;
 
     public ParkingBoy(ParkingLot parkingLot) {
@@ -20,12 +23,12 @@ public class ParkingBoy implements Parkable {
     @Override
     public Car fetchCar(Ticket ticket) {
         if (ticket == null) {
-            System.out.print("Please provide your parking ticket.\n");
+            System.out.print(NONE_PARKING_TICKET);
             return null;
         }
         Optional<Car> optionalCar = parkingLots.stream().map(parkingLot -> parkingLot.fetchCar(ticket)).filter(Objects::nonNull).findFirst();
         if (!optionalCar.isPresent()) {
-            System.out.print("Unrecognized parking ticket.\n");
+            System.out.print(UNRECOGNIZED_PARKING_TICKET);
         }
         return optionalCar.orElse(null);
     }
@@ -33,7 +36,7 @@ public class ParkingBoy implements Parkable {
     @Override
     public Ticket park(Car car) {
         if (isParkingLotsFull()) {
-            System.out.print("Not enough position.\n");
+            System.out.print(NOT_ENOUGH_POSITION);
         }
         Optional<ParkingLot> optionalParkingLot = parkingLots.stream().
                 filter(parkingLot -> parkingLot.getCarsCount() < parkingLot.getCapacity()).
